e16b381ffaf1bf9232f6736ec7dd05662762b131
abdul khaliq <abdallamakhtal@gmail.com>
2024-07-04 17:13:24 +0300

    complete labs

---
+++ index.js
@@ -0,0 +1,13 @@
+const divide = (parameter1,parameter2) => { 
+console.log(`dividing ${parameter1}`);
+console.log(`dividing ${parameter2}`);
+return parameter1 / parameter2;
+}
+divide("1 / 5")
+
+const square = (num) => {
+    return num * num;
+}
+
+const add =(a,b) => a + b;
+
